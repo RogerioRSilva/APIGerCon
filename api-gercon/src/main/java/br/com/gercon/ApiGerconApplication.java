ba1ddfb49feff4e069dd6fb42d70e7ef07cd1b80
RogerioRSilva <rogrodriguessilva2022@gmail.com>
2024-10-04 15:29:55 -0300

    Criado ReadMe e atualizado

---
+++ api-gercon/src/main/java/br/com/gercon/ApiGerconApplication.java
@@ -0,0 +1,13 @@
+package br.com.gercon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerconApplication.class, args);
+	}
+
+}
